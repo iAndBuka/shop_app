4a55edf5af37a710665befe829000624010e43f7
raulpsingh <raulpsingh2@gmail.com>
2022-02-07 13:56:33 +0200

    update

---
+++ shopApp/lib/services/database_service.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shop_app/objects/user.dart';
+
+class AuthService {
+  final FirebaseAuth _fAuth = FirebaseAuth.instance;
+
+
+
+
+}
