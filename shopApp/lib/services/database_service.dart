b3bc4e474ae62898d3b0da1c88051ce4907a1690
raulpsingh <raulpsingh2@gmail.com>
2022-02-07 14:10:41 +0200

    Firebase Auth added to code

--- shopApp/lib/services/database_service.dart
+++ shopApp/lib/services/database_service.dart
@@ -5,7 +5,39 @@ import 'package:shop_app/objects/user.dart';
 class AuthService {
   final FirebaseAuth _fAuth = FirebaseAuth.instance;
 
+  Future<AppUser?> signInWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      UserCredential result = await _fAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+      return AppUser.fromFirebase(user!);
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
 
+  Future<AppUser?> registerWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      UserCredential result = await _fAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+      return AppUser.fromFirebase(user!);
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
 
+  Future logOut() async {
+    await _fAuth.signOut();
+  }
 
+  Stream<AppUser?> get currentUser {
+    return _fAuth
+        .authStateChanges()
+        .map((User? user) => user != null ? AppUser.fromFirebase(user) : null);
+  }
 }
